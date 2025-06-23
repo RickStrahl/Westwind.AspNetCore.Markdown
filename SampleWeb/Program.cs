1f44b9ad426a568193320751590f1b98a8e091d4
Rick Strahl <rstrahl@west-wind.com>
2025-06-23 10:56:39 -0700

    Refactor Sample to use newer program.cs startup syntax.

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -1,18 +1,83 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using System.Collections.Generic;
+using Markdig;
+using Markdig.Extensions.AutoIdentifiers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Westwind.AspNetCore.Markdown;
 
-namespace SampleWeb
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMarkdown(config =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+    config.HtmlTagBlackList = "script|iframe|object|embed|form";
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+    config.MarkdownRenderExtensions.Add(new PlantUmlMarkdownRenderExtension());
+    config.MarkdownRenderExtensions.Add(new FontAwesomeRenderExtension());
+
+    var folderConfig = config.AddMarkdownProcessingFolder("/docs/", "~/Pages/__MarkdownPageTemplate.cshtml");
+    folderConfig = config.AddMarkdownProcessingFolder("/posts/", "~/Pages/__MarkdownPageTemplate.cshtml");
+
+    folderConfig.SanitizeHtml = false;
+    folderConfig.ProcessExtensionlessUrls = true;
+    folderConfig.ProcessMdFiles = true;
+
+    folderConfig.PreProcess = (model, controller) =>
+    {
+        // controller.ViewBag.Model = new MyCustomModel();
+    };
+
+    config.ConfigureMarkdigPipeline = builder =>
+    {
+        builder.UseEmphasisExtras(Markdig.Extensions.EmphasisExtras.EmphasisExtraOptions.Default)
+            .UsePipeTables()
+            .UseGridTables()
+            .UseAutoIdentifiers(AutoIdentifierOptions.GitHub)
+            .UseAutoLinks()
+            .UseAbbreviations()
+            .UseYamlFrontMatter()
+            .UseEmojiAndSmiley(true)
+            .UseListExtras()
+            .UseFigures()
+            .UseTaskLists()
+            .UseCustomContainers()
+            //.DisableHtml()
+            .UseGenericAttributes();
+    };
+});
+
+builder.Services.AddMvc()
+    .AddApplicationPart(typeof(MarkdownPageProcessorMiddleware).Assembly)
+    .AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseDefaultFiles(new DefaultFilesOptions()
+{
+    DefaultFileNames = new List<string> { "index.md", "index.html" }
+});
+
+app.UseMarkdown();
+
+app.UseRouting();
+
+app.UseStaticFiles();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapDefaultControllerRoute();
+});
+
+app.Run();
